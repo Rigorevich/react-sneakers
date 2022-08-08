7bc79f2c33bf409f1e6d43e1e0186f888e9ef6d0
Rigorevich <rigorevich268@gmail.com>
2022-08-09 01:49:34 +0300

    finish the project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+
 import './index.scss';
 import 'macro-css';
+
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+    <React.StrictMode>
+        <Router>
+            <App />
+        </Router>
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
